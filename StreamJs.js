7b4e8c5a84018ac3c9ae474139d5b7861f32c82a
marsbotz <aalam04042002@gmail.com>
2025-05-29 12:08:09 +0530

    Update StreamJs.js

--- StreamJs.js
+++ StreamJs.js
@@ -263,7 +263,7 @@ document.addEventListener('keydown', function (e) {
 const videolink = window.location.href;
 const streamlink = videolink.replace("/watch/", "/dl/");
 
-/*function vlc_player() {
+function vlc_player() {
     const openstreamlink = streamlink;
     const openVlc = `vlc://${openstreamlink}`;
     window.location.href = openVlc;
@@ -297,7 +297,7 @@ function hd_player() {
     const openstreamlink = streamlink;
     const openHDplayer = `intent:${openstreamlink}#Intent;package=uplayer.video.player;end`;
     window.location.href = openHDplayer;
-}*/
+}
 const playerUrlBuilder = {
     //'vlc-pc': url => `vlc://${url}`,
     //'potplayer': url => `potplayer://${url}`,
